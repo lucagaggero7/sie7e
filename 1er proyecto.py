0e443ed23cad74f8f2e005ccdacba0d76f4e4f7e
Stefano Gaggero <63757098+lucagaggero7@users.noreply.github.com>
2021-08-18 14:13:28 -0300

    Add files via upload

---
+++ 1er proyecto.py
@@ -0,0 +1,11 @@
+edad_necesaria_para_conducir = 18
+tu_nombre = (input ("COMO TE LLAMAS NENE?"))
+tu_edad = int (input ("CUANTOS AÑOS TENES?"))
+
+if tu_edad > edad_necesaria_para_conducir:
+    print ("FELICITACIONES", (tu_nombre), "ERES APTO PARA CONDUCIR")
+
+if tu_edad < edad_necesaria_para_conducir:
+    print("LO SIENTO", (tu_nombre) ,"AUN TE FALTAN",(edad_necesaria_para_conducir - tu_edad),"AÑO/S PARA CONDUCIR")
+
+input("Presione enter para continuar... :D")
